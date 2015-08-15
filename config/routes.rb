b3a82534be0e90b303d49098385f5c3fae576ded
YLHii <yonglian146@gmail.com>
2015-08-15 21:01:03 +0800

    Added gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :forums
   resources :posts
-  devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+
   root to: 'static_pages#home'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
